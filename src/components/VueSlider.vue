7d969c8204b34ce489bc5273d359dd54e4c92201
edu <edu@localhost.localdomain>
2019-05-18 09:22:04 -0400

    fixed vue carousel design

--- src/components/VueSlider.vue
+++ src/components/VueSlider.vue
@@ -1,35 +1,116 @@
 <template>
-        <div class="alsdi-vue-slider">
-        <carousel>
-            <slide>
-                Slide 1 Content
-            </slide>
-            <slide>
-                Slide 2 Content
-            </slide>
-            <slide>
-                Slide 1 Content
-            </slide>
-            <slide>
-                Slide 2 Content
-            </slide>
-        </carousel>
+  <div class="alsdi-vue-slider text-center">
+    <carousel :perPageCustom="[[320, 1],[768, 3], [1440, 4]]" :loop="true" :centerMode="true" :autoplay="true" :paginationEnabled="false">
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+      <slide>
+        <div class="card">
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel';
+import { Carousel, Slide } from "vue-carousel";
 export default {
-    components: {
-        Carousel,
-        Slide
-    }
-}
+  components: {
+    Carousel,
+    Slide
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .alsdi-vue-slider {
-    background-color: #fff;
+    .card {
+        margin: 15px;
+    }
 }
 </style>
 
