7ac7349f310b3654a7fd8bf1f80c84b069192ac8
edu <edu@localhost.localdomain>
2019-05-22 11:04:01 -0400

    added background image to body

--- src/App.vue
+++ src/App.vue
@@ -33,8 +33,24 @@ export default {
 @import url("//www.fontstatic.com/f=vip-hakm-bold");
 body {
   font-family: 'vip-hakm-bold', Helvetica, Arial, sans-serif !important;
-  background-color: #333 !important;
+  background-color: rgba(0, 0, 0, .6) !important;
   overflow-x: hidden;
+  background-image: url("./assets/bg-img.jpeg");
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: 50%;
+  position: relative;
+  &::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, .7)
+  }
 }
 #app {
   -webkit-font-smoothing: antialiased;
