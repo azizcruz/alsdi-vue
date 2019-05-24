ff043a875c7820f1592b57f12ff02425f128097c
edu <edu@localhost.localdomain>
2019-05-24 09:18:48 -0400

    added manager section

--- src/components/AboutUsManagerWord.vue
+++ src/components/AboutUsManagerWord.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="about-us-manager-word text-right">
     <h3 class="mt-3 mb-3 font-weight-bold">كلمة من المدير العام</h3>
-    <h1>-تحت التنفيذ-</h1>
+    <div
+      class="manager-word d-flex flex-wrap"
+      :style="{
+        backgroundImage: `url('${require('@/assets/manager-img.jpg')}')`,
+        backgroundPosition: 'center'
+      }"
+    >
+      <div class="manager-image col-sm-12 col-md-8"></div>
+      <div class="manager-text text-justify text-right col-sm-12 col-md-4 p-3 pl-5">
+        <h3>Lorem ipsum dolor sit.</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero
+          deserunt voluptatibus, dolorum, voluptate et natus voluptatem iure
+          officia voluptatum at dolor illo perspiciatis ex quas dolore, esse
+          harum inventore corporis. Quod iure adipisci, architecto iusto vero
+          pariatur ipsum blanditiis quis sint repudiandae expedita excepturi
+          unde laudantium necessitatibus ex, quibusdam tenetur.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +28,25 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/_colors.scss";
+@import "@/assets/_alsdi-framework.scss";
+
+.about-us-manager-word {
+  background-size: contain;
+  background-repeat: no-repeat;
+  .manager-image {
+    @media screen and (max-width: 768px){
+      min-height: 400px;
+    }
+  }
+  .manager-text {
+    min-height: 600px;
+    background-color: $alsdi-black;
+    color: $alsdi-gold;
+    border-top-left-radius: 30%;
+    // border-bottom-left-radius: 30%;
+  }
+}
 </style>
+
