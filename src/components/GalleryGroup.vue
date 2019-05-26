8324ad99a7b04358d5e2acab9d5cd79c350db0c2
edu <edu@localhost.localdomain>
2019-05-26 14:10:37 -0400

    fixed gallery show

--- src/components/GalleryGroup.vue
+++ src/components/GalleryGroup.vue
@@ -1,132 +1,23 @@
 <template>
   <div class="gallery-group">
-    <gallery :images="images" :index="index" @close="index = null"></gallery>
-    <div class="d-flex flex-column flex-lg-row flex-lg-wrap h-100">
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
-      </div>
-      <div class="col-2 d-flex justify-content-center justify-content-lg-start">
-        <div class="images-group">
-          <div
-            class="image"
-            v-for="(image, imageIndex) in images"
-            :key="imageIndex"
-            @click="index = imageIndex"
-            :style="{
-              backgroundImage: 'url(' + image + ')',
-              width: '100px',
-              height: '100px',
-              zIndex: imageIndex,
-              marginLeft: imageIndex / 3 + 'rem',
-              marginTop: imageIndex / 3 + 'rem'
-            }"
-          ></div>
-        </div>
+    <div class="d-flex flex-wrap justify-content-between align-items-between">
+      <gallery :images="currentImages" :index="index" @close="index = null"></gallery>
+      <div
+        class="b col-sm-12 col-md-6 col-lg-3 d-flex justify-content-center align-items-center"
+        v-for="(ir, ind) in imagesWrapper" :key="ind"
+      >
+        <div
+          class="image"
+          v-for="(image, imageIndex) in ir.images"
+          @click="setCurrentImages(imageIndex, ir.images)"
+          :key="imageIndex"
+          :style="{
+            backgroundImage: 'url(' + image + ')',
+            zIndex: imageIndex,
+            top: imageIndex / 2 + 'rem',
+            left: imageIndex / 2 + 'rem'
+          }"
+        ></div>
       </div>
     </div>
   </div>
@@ -137,18 +28,39 @@ import VueGallery from "vue-gallery";
 export default {
   data: function() {
     return {
-      images: [
-        "https://dummyimage.com/800/ffffff/000000",
-        "https://dummyimage.com/1600/ffffff/000000",
-        "https://dummyimage.com/1280/000000/ffffff",
-        "https://dummyimage.com/400/000000/ffffff"
+      imagesWrapper: [
+        {
+          images: [
+            "https://dummyimage.com/800/ffffff/000000",
+            "https://dummyimage.com/1600/ffffff/000000",
+            "https://dummyimage.com/1280/000000/ffffff",
+            "https://dummyimage.com/400/000000/ffffff"
+          ],
+          text: "lorem 10"
+        },
+        {
+          images: [
+            "https://dummyimage.com/800/ff4fff/000000",
+            "https://dummyimage.com/1600/ffffff/000000",
+            "https://dummyimage.com/1280/000000/ffffff",
+            "https://dummyimage.com/400/000000/ffffff"
+          ],
+          text: "lorem 10"
+        }
       ],
+      currentImages: [],
       index: null
     };
   },
 
   components: {
     gallery: VueGallery
+  },
+  methods: {
+    setCurrentImages(imageIndex, currentImages) {
+      this.index = imageIndex
+      this.currentImages = currentImages
+    }
   }
 };
 </script>
@@ -157,18 +69,40 @@ export default {
 @import "@/assets/_alsdi-framework.scss";
 @import "@/assets/_colors.scss";
 .gallery-group {
-  padding: 20px;
-  cursor: pointer;
-  .h-100 {
-    height: 100vh !important;
+  padding: 10px;
+  .blueimp-gallery-display {
+    display: none;
   }
-  .images-group {
+  cursor: pointer;
+  .b {
     position: relative;
+    padding: 10px;
     .image {
       position: absolute;
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center center;
+      width: 80%;
+      height: 80%;
+    }
+    // Small devices (landscape phones, 576px and up)
+    @media (min-width: 320px) {
+      min-height: 80vw;
+    }
+
+    // Medium devices (tablets, 768px and up)
+    @media (min-width: 768px) {
+      min-height: 35vw;
+    }
+
+    // Large devices (desktops, 992px and up)
+    @media (min-width: 992px) {
+      min-height: 18vw;
+    }
+
+    // Extra large devices (large desktops, 1200px and up)
+    @media (min-width: 1200px) {
+      min-height: 18vw;
     }
   }
 }
