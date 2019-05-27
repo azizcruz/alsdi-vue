0b6febe1d27efa5497891895dbee09e65eaa135c
edu <edu@localhost.localdomain>
2019-05-27 10:12:18 -0400

    added animation to gallery album

--- src/components/GalleryGroup.vue
+++ src/components/GalleryGroup.vue
@@ -80,34 +80,49 @@ export default {
 @import "@/assets/_alsdi-framework.scss";
 @import "@/assets/_colors.scss";
 
-
-
 .gallery-group {
   padding: 10px;
   .blueimp-gallery-display {
     display: none;
   }
   .blueimp-gallery {
-      .close,
-      .next,
-      .prev {
-        color: $alsdi-white !important;
+    .close,
+    .next,
+    .prev {
+      color: $alsdi-white !important;
 
-        &:hover {
-          color: $alsdi-gold !important;
-        }
+      &:hover {
+        color: $alsdi-gold !important;
       }
+    }
   }
   cursor: pointer;
   .b {
     position: relative;
     padding: 10px;
+    &:hover {
+      .image {
+        &:nth-child(1) {
+          margin: 10px;
+        }
+        &:nth-child(2) {
+          margin: 20px;
+        }
+        &:nth-child(3) {
+          margin: 30px;
+        }
+      }
+    }
+
     .image {
       position: absolute;
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center center;
       border-radius: 15px;
+      -webkit-transition: margin 0.3s ease-in-out;
+      -o-transition: margin 0.3s ease-in-out;
+      transition: margin 0.3s ease-in-out;
       .album-name {
         background-color: $alsdi-gold;
         padding: 10px;
