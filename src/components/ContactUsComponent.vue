f3ccebeabc608fb5d15ec2185e13e6ad501fb962
edu <edu@localhost.localdomain>
2019-05-29 11:58:05 -0400

    added designs and map

--- src/components/ContactUsComponent.vue
+++ src/components/ContactUsComponent.vue
@@ -65,23 +65,37 @@
           <div class="text-center">
             <h3><i class="fas fa-phone-volume"></i> ارقام هواتفنا</h3>
             <p>22454730 - 22454733</p>
-            <br>
+            <br />
             <h3><i class="far fa-envelope"></i> عنوان الايميل</h3>
             <p>test@test.com</p>
-            <br>
+            <br />
             <h3><i class="fas fa-map-marker-alt"></i> العنوان</h3>
             <p>Kuwait-Stadt, Kuwait</p>
-            <br>
+            <br />
             <h3><i class="fas fa-globe"></i> الموقع</h3>
             <p><a href="https://alsdi.com">www.alsdi.com</a></p>
           </div>
         </div>
       </div>
       <div
-        class="contact-us-tab map-section container"
+        class="contact-us-tab map-section container text-center"
         v-show="activeTab === 3"
       >
-        Map
+        <h3 class="">اضغط على الصورة للتوجه للعنوان</h3>
+        <div class="d-flex justify-content-center address-image-wrapper">
+          <div class="test">
+            <a
+              href="https://www.google.com/maps/place/%D9%85%D9%83%D8%AA%D8%A8+%D8%A3%D8%B3%D8%A7%D9%85%D8%A9+%D8%A7%D9%84%D8%B5%D8%AF%D9%8A+%D9%84%D9%84%D8%A7%D8%B3%D8%AA%D8%B4%D8%A7%D8%B1%D8%A7%D8%AA+%D8%A7%D9%84%D9%87%D9%86%D8%AF%D8%B3%D9%8A%D8%A9+Al-Sadi+Engineering+Consultant%E2%80%AD/@29.3786671,47.9950493,17.75z/data=!4m12!1m6!3m5!1s0x3fcf85caa39d288f:0x762a3eca2a9438c0!2z2YXZg9iq2Kgg2KPYs9in2YXYqSDYp9mE2LXYr9mKINmE2YTYp9iz2KrYtNin2LHYp9iqINin2YTZh9mG2K_Ys9mK2KkgQWwtU2FkaSBFbmdpbmVlcmluZyBDb25zdWx0YW50!8m2!3d29.378913!4d47.995612!3m4!1s0x3fcf85caa39d288f:0x762a3eca2a9438c0!8m2!3d29.378913!4d47.995612?hl=en-KW"
+              target="_blank"
+            >
+              <img
+                src="@/assets/location-img.jpeg"
+                class="img-fluid alsdi-hoverable"
+                alt="عنوان الصدي للاستشارات الهندسية"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -115,6 +129,42 @@ export default {
 
   .contact-us-tab {
     height: 80vh;
+    h3 {
+      display: inline-block;
+      &::after {
+        content: "";
+        display: block;
+        width: 0;
+        margin-top: 10px;
+        height: 2px;
+        background: #000;
+        transition: width 0.3s;
+      }
+    }
+    &:hover {
+      h3 {
+        &::after {
+          width: 100%;
+          //transition: width .3s;
+        }
+      }
+      img {
+        -webkit-transform: scale(0.9);
+        -ms-transform: scale(0.9);
+        transform: scale(0.9);
+      }
+    }
+    .address-image-wrapper {
+      a {
+        img {
+          border-radius: 8px;
+          position: relative;
+          -webkit-transition: all 0.5s ease-in-out;
+          -o-transition: all 0.5s ease-in-out;
+          transition: all 0.5s ease-in-out;
+        }
+      }
+    }
   }
 
   .contact-form {
@@ -122,7 +172,6 @@ export default {
   }
 
   .form-info-control {
-
     & > div {
       background-color: $alsdi-black;
       color: $alsdi-gold;
